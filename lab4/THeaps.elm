3b0821d4a85b9592e70f8f41c31534bdb62bb222
Nolan Cretney <nokynokes@gmail.com>
2018-02-27 13:25:08 -0700

    finished insert

--- lab4/THeaps.elm
+++ lab4/THeaps.elm
@@ -41,6 +41,7 @@ findMin h =
   case h of
     Heap (0, _) -> Nothing
     Heap (_, (Node a _ _) ) -> Just a
+    _ -> Debug.crash "TODO"
 
 insert : comparable -> Heap comparable -> Heap comparable
 insert x h =
@@ -48,10 +49,42 @@ insert x h =
     Heap (n, t) ->
       if n==0
       then Heap (1, Node x Empty Empty)
-      else Heap (n+1, )
+      else Heap (n+1, insertAndBubbleUp x (pathTo (parentIndex n)) t)
 
 insertAndBubbleUp : comparable -> List Dir -> Tree comparable -> Tree comparable
 insertAndBubbleUp x dirList t =
+  case (dirList, (unwrapNode t)) of
+    ([], (d, l, r)) ->
+      if d < x then
+        case (l,r) of
+          (Empty, _) -> Node d (Node x Empty Empty) r
+          (_, Empty) -> Node d l (Node x Empty Empty)
+          _ -> Debug.crash "should never hit this case"
+      else
+        case (l,r) of
+          (Empty, _) -> Node x (Node d Empty Empty) r
+          (_, Empty) -> Node x l (Node d Empty Empty)
+          _ -> Debug.crash "should never hit this case"
+    (Right :: t, (d, l ,r)) ->
+      let rTree = insertAndBubbleUp x t r in
+        case unwrapNode rTree of
+          (xp, lp, rp) ->
+            if d < xp then
+              Node d l rTree
+            else
+              Node xp l (Node d lp rp)
+    (Left :: t, (d , l, r)) ->
+      let lTree = insertAndBubbleUp x t l in
+        case unwrapNode lTree of
+          (xp, lp, rp) ->
+            if d < xp then
+              Node d lTree r
+            else
+              Node xp (Node d lp rp) r
+
+
+
+
 
 
 
